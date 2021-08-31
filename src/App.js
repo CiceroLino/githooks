4604a7a3eb6ee01eec1e759347902e2ce2fda626
Cicero Lino <cicerolinoeneto@gmail.com>
2021-08-31 18:13:26 -0300

    V1 githooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React, { useLayoutEffect, useState } from "react";
+import React, { useLayoutEffect, useState, useEffect } from "react";
 
 export default function App() {
-  const [repositories, setRepositories] = useState([
-    { id: 1, name: 'repo-teste-1' },
-    { id: 2, name: 'repo-teste-2' },
-    { id: 3, name: 'repo-teste-3' },
-  ]);
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(async () => {
+    // Criar um tipo de entrada para que ocupe o valor do const response para consultar outros tipos de repositórios
+    const response = await fetch('http://api.github.com/users/CiceroLino/repos');
+    const data = await response.json();
+
+    setRepositories(data);
+  }, []);
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+
+    document.title = `Você tem ${filtered.length} favoritos`;
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ?  { ... repo, favorite: !repo.favorite } : repo
+    });
+
+    setRepositories(newRepositories);
+  }
 
   return (
-    <ul>
-      {repositories.map(repo => (
-        <li key={repo.id}>{repo.name}</li>
-      ))}
-    </ul>
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.name}
+            {repo.favorite && <span>(Favorito)</span>}
+            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          </li>
+        ))}
+      </ul>
   );
 }
