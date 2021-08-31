70e137600b93389cb3feeea5ba53a2f6529edbd3
Cicero Lino <cicerolinoeneto@gmail.com>
2021-08-31 19:10:04 -0300

    V2 githooks

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
-import React, { useLayoutEffect, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
-  const [repositories, setRepositories] = useState([]);
+  const [location, setLocation] = useState({});
 
-  useEffect(async () => {
-    // Criar um tipo de entrada para que ocupe o valor do const response para consultar outros tipos de repositórios
-    const response = await fetch('http://api.github.com/users/CiceroLino/repos');
-    const data = await response.json();
-
-    setRepositories(data);
+  useEffect (() => {
+    navigator.geolocation.watchPosition(handlePositionRecieved)
   }, []);
+  
+  function handlePositionRecieved({ coords }) {
+    const { latitude, longitude } = coords;
 
-  useEffect(() => {
-    const filtered = repositories.filter(repo => repo.favorite);
-
-    document.title = `Você tem ${filtered.length} favoritos`;
-  }, [repositories]);
-
-  function handleFavorite(id) {
-    const newRepositories = repositories.map(repo => {
-      return repo.id === id ?  { ... repo, favorite: !repo.favorite } : repo
-    });
-
-    setRepositories(newRepositories);
+    setLocation({ latitude, longitude });
   }
 
   return (
-      <ul>
-        {repositories.map(repo => (
-          <li key={repo.id}>
-            {repo.name}
-            {repo.favorite && <span>(Favorito)</span>}
-            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
-          </li>
-        ))}
-      </ul>
+      <>
+        Latitude: {location.latitude} <br />
+        Longitude: {location.longitude}
+      </>
   );
 }
